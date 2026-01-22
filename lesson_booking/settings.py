0de72087cac47e4981e08d05b5062272e1e6c24e
Muthukumarlaxman Ramanathan Ekappan <mklre@Muthukumarlaxmans-MacBook-Pro.local>
2026-01-22 23:00:35 +1030

    Add Postgres support

--- lesson_booking/settings.py
+++ lesson_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +78,14 @@ WSGI_APPLICATION = 'lesson_booking.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+      #  'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
+
 
 
 # Password validation
