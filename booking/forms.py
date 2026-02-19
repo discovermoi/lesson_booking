bf1d036bf23cf8b7b393a5078f92d9187e8ef9b3
mklre <discovermoi@hotmail.com>
2026-02-19 23:36:15 +1030

    mobile responsive UI loginhtml

--- booking/forms.py
+++ booking/forms.py
@@ -1,7 +1,20 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 from .models import Booking
 from users.models import Profile
 
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "w-full border rounded-md px-3 py-2 focus:ring-2 focus:ring-indigo-500"
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "w-full border rounded-md px-3 py-2 focus:ring-2 focus:ring-indigo-500"
+        })
+    )
 
 class BookingForm(forms.ModelForm):
     class Meta:
